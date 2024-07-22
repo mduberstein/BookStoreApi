9ab6e55cad91b9aa5b209141e8c59f8f4a5726e0
Michael Duberstein <mduberstein@gmail.com>
2024-07-21 23:10:50 -0400

    Configrured JSON Serialization Options

--- BookStoreApi/Program.cs
+++ BookStoreApi/Program.cs
@@ -7,7 +7,8 @@ var builder = WebApplication.CreateBuilder(args);
 var serviceCollection = builder.Services.Configure<BookStoreDatabaseSettings>(builder.Configuration.GetSection("BookStoreDatabase"));
 serviceCollection = builder.Services.AddSingleton<BooksService>();
 var service = serviceCollection[serviceCollection.Count - 1];
-var mvcBuilder = builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddJsonOptions(options => options.JsonSerializerOptions.PropertyNamingPolicy = null);
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
